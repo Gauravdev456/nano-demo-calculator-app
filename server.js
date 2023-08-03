d063882b58e6923a35f5c0f7000b3b71fc682d15
Gauravdev HB <gauravdev818@gmail.com>
2023-08-03 20:44:52 +0530

    1

--- server.js
+++ server.js
@@ -10,19 +10,21 @@ app.use(express.json());
 const baseRouter = express.Router();
 
 baseRouter.get('/greeting', (req, res) => {
+    return res.send('Hello World!');
     return res.send('');
 });
 
 baseRouter.post('/add', (req, res) => {
+    const { first, second } = req.body;
+    res.json({ "result": first + second });
     res.json({ "": null });
 });
 
 
 baseRouter.post('/subtract', (req, res) => {
+    const { first, second } = req.body;
+    res.json({ "result": first - second });
     res.json({ "": null });
 });
 
 app.use(baseUrl, baseRouter);
-app.listen(PORT, () => {
-    console.log("Server running at PORT", PORT);
-});
